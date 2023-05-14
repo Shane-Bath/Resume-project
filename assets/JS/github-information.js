0ad0645972a294941004537fdedabdf6e72d5aef
Shane <77701559+Shane-Bath@users.noreply.github.com>
2023-05-14 13:40:52 +0000

    add fetch repo info for users

--- assets/JS/github-information.js
+++ assets/JS/github-information.js
@@ -15,6 +15,27 @@ function userInformationHTML(user) {
         </div>`;
 }
 
+function repoInformationHTML(repos) {
+    if(repos.length == 0) {
+        return `<div class="clearfix repo-list">No repos!</div>`;
+    }
+
+    var listItemHTML = repos.map(function(repo) {
+        return `<li>
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`;
+    });
+
+    return `<div class="clearfix repo-list">
+                <p>
+                    <strong>Repo List: </strong>
+                </p>
+                <ul>
+                    ${listItemHTML.join("\n")}
+                </ul>
+    
+            </div>`;
+}
 
 function fetchGitHubInformation(event) {
 
@@ -37,7 +58,7 @@ function fetchGitHubInformation(event) {
             var userData = firstResponse[0];
             var repoData = secondResponse[0];
             $("#gh-user-data").html(userInformationHTML(userData));
-            $("#gh-user-data").html(repoInformationHTML(repoData));
+            $("#gh-repo-data").html(repoInformationHTML(repoData));
         }, function(errorResponse) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(
